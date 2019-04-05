7c334e17867bb7b4f7cb604a3d4416c1f3d617c7
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-04-04 21:49:13 -0400

    Update ksac-packer-build.sh

--- ksac-packer-build.sh
+++ ksac-packer-build.sh
@@ -1,1 +1,1 @@
-packer build -var 'aws_access_key=AKIAJWMXJ2EHO5Q33TAQ' -var 'aws_secret_key=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' ksac-packer.json
+packer build -var 'aws_access_key=<Key Value>' -var 'aws_secret_key=<Key Value>' ksac-packer.json
