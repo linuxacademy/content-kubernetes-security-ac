b4877ce2478b3c2a3db35bd79366890d6463b7bb
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-04-29 15:50:32 -0400

    Create k8s-create-ac.sh

---
+++ k8s-create-ac.sh
@@ -0,0 +1,15 @@
+export AWS_HOSTED_ZONE="$(aws route53 list-hosted-zones | jq '.HostedZones[0] .Name' | tr -d '"' | sed 's/\.$//' )"
+echo "Hosted Zone:" $AWS_HOSTED_ZONE
+echo "CREATING S3 BUCKET"
+echo "=================="
+aws s3 mb s3://k8s3.$AWS_HOSTED_ZONE
+export KOPS_STATE_STORE=s3://k8s3.$AWS_HOSTED_ZONE
+export KOPS_CLUSTER_NAME=k8s.$AWS_HOSTED_ZONE
+echo "CREATING RSA KEY"
+echo "================="
+ssh-keygen -q -f ./.ssh/id_rsa -N ''
+echo "CREATING CLUSTER"
+echo "================="
+kops create cluster --master-size=t2.medium --zones=us-east-1c --name=$KOPS_CLUSTER_NAME --cloud aws\
+  --authorization rbac --topology private --networking canal --bastion --image ami-74e6b80d \
+  --ssh-public-key ~/.ssh/id_rsa.pub 
