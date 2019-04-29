a36d26e9b25276a0a07ba010ed52fe7a050372cd
John Marx <42839573+johnmarx-la@users.noreply.github.com>
2019-04-29 18:08:16 -0400

    Update k8s-create-ac.sh

--- k8s-create-ac.sh
+++ k8s-create-ac.sh
@@ -11,4 +11,4 @@ ssh-keygen -q -f ./.ssh/id_rsa -N ''
 echo "CREATING CLUSTER"
 echo "================="
 kops create cluster --master-size=t2.medium --zones=us-east-1c --name=$KOPS_CLUSTER_NAME --cloud aws\
-  --authorization rbac --topology private --networking canal --ssh-public-key ~/.ssh/id_rsa.pub 
+  --authorization rbac --topology private --bastion --networking canal --ssh-public-key ~/.ssh/id_rsa.pub 
